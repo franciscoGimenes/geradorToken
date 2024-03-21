55a35b0fff9f5974988f6143494d5d4ca3b53bd1
Gimenes <f.gimenes1316@gmail.com>
2024-03-21 08:47:03 -0300

    att

--- components/modal.js
+++ components/modal.js
@@ -1,19 +1,28 @@
 import { View, StyleSheet, Pressable, Text, TouchableOpacity } from "react-native";
+import * as Clipboard from 'expo-clipboard'
 
-export function ModalTokens({handleClose}) {
+
+export function ModalTokens({token, fechar}) {  
+
+    async function copiarToken() {
+        await Clipboard.setStringAsync(token)
+        alert("Token copiado para área de transferência.")
+        fechar();
+    }
+
     return (
         <View style={ESTILO.container}>
             <View style={ESTILO.content}>
                 <Text style={ESTILO.title}>
                     Senha Gerada
                 </Text>
-                <Pressable style={ESTILO.innerToken} >
-                    <Text style={ESTILO.text}>
-                        senha
+                <Pressable style={ESTILO.innerToken} onLongPress={copiarToken} >
+                    <Text selectable={false} style={ESTILO.text}>
+                        {token}
                     </Text>
                 </Pressable>
                 <View style={ESTILO.buttonArea}>
-                    <TouchableOpacity style={ESTILO.button} onPress={handleClose}>
+                    <TouchableOpacity style={ESTILO.button} onPress={fechar}>
                         <Text style={ESTILO.buttonText} >
                             Voltar
                         </Text>
