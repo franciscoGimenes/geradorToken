0206a3274ef7d970e1ee82c54409cee5fb7b0a79
Gimenes <f.gimenes1316@gmail.com>
2024-03-21 11:38:37 -0300

    att

--- pages/paginaSenhas.js
+++ pages/paginaSenhas.js
@@ -1,13 +1,48 @@
-import { View, StyleSheet, Text} from "react-native";
+
+import { View, StyleSheet, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Armazenamento from '../hooks/bancoTokens';
+import { useState, useEffect } from 'react';
+import { CaixaToken } from '../components/tokenView';
+import { useIsFocused } from '@react-navigation/native';
 
 export function PaginaSenhas() {
+    const { obterItem, removerItem } = Armazenamento();
+    const [listaTokens, defListaTokens] = useState([]);
+    const telaAtiva = useIsFocused();
+
+
+    async function deletarToken(item) {
+        const tokens = await removerItem("@pass", item)
+        defListaTokens(tokens)
+    };
+
+    useEffect(() => {
+        async function carregaTokens() {
+            const tokens = await obterItem("@token");
+            defListaTokens(tokens);
+        }
+        carregaTokens()
+    }, [telaAtiva]);
+
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={ESTILO.header}>
                 <Text style={ESTILO.title}>
                     Minhas senhas
                 </Text>
+            </View>
+            <View style={ESTILO.content}>
+                <FlatList
+                    style={{ flex: 1, paddingTop: 14, }}
+                    data={listaTokens}
+                    keyExtractor={(item) => String(item)}
+                    renderItem={({ item }) => <CaixaToken
+                        data={item}
+                        removePassword={() => deletarToken(item)}
+                    />}
+                />
             </View>
         </SafeAreaView>
     )
@@ -23,6 +58,11 @@ const ESTILO = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         color: "#FFF"
-    }
+    },
+    content: {
+        flex: 1,
+        paddingLeft: 14,
+        paddingRight: 14,
+    },
 
 })
